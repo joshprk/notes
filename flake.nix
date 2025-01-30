6929751477060dd2cc1adacc1ef03c2d6fccb1b1
Joshua Park <joshuprk@gmail.com>
2025-01-30 13:01:55 -0500

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs
+  }: let
+    lib = nixpkgs.lib;
+    forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    getPkgs = system: import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = getPkgs system;
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          quartoMinimal
+        ];
+      };
+    });
+  };
+}
